145058089af8361fc6f502d972f8fa530a7f22ee
Pingblue <custard@mailfence.com>
2025-10-07 16:27:13 +0100

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,7 +4,7 @@
 // This plugin will try to load "trackers.lst" file from 'share/users/<user>/settings/' directory first
 // If file is not found then it will load "trackers.lst" from plugin directory
 
-$MAX_UL_LIMIT 	= 1;		// 1 Kb/s
+$MAX_UL_LIMIT 	= 1;		// 1 KiB/s
 $MAX_DL_LIMIT 	= 0;		// unlimited
 
 $RATIO_LIMIT	= 0;		// percents
